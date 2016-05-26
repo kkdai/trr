97b0c945296d1aef3b22bfe4fda7844e251498b8
Evan Lin <evanslin@gmail.com>
2016-05-27 00:28:15 +0800

    fix test build break

--- pubsub_test.go
+++ pubsub_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestBasic(t *testing.T) {
-	tsb := NewTszPubsub()
+	tsb := NewTszPubsub(10)
 	tsb.PublishTimeData("d1", 0, 0)
 	tsb.PublishTimeData("d1", 0, 0)
 }
 
 func TestBasicRead(t *testing.T) {
-	tsb := NewTszPubsub()
+	tsb := NewTszPubsub(10)
 	tsb.PublishTimeData("d1", 3122, 11102)
 	tt, v, err := tsb.ReadChanTopic("d1")
 	if err != nil || tt != uint32(3122) || v != float64(11102) {
